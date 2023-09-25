86a12f29caeb177cb5eb8ebf8629050f21407616
zelzmiy <adambelski2@gmail.com>
2023-09-25 11:46:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TEMEliminatesMonsters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TEM game = new TEM();
+            game.Window.Title = "TEM Eliminates Monsters";
+            game.Run();
+        }
+    }
+}
---
+++ TEM.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TEMEliminatesMonsters
+{
+    public class TEM : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public TEM()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime) // Note, do NOT draw stuff in here
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) // Note, do NOT move gameobjects in here 
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
