c4ce84f3bc25597c9789bc37a9bafefc3df85eb4
zelzmiy <adambelski2@gmail.com>
2023-10-10 12:01:58 -0500

    ermmme image!!!

--- TEM.cs
+++ TEM.cs
@@ -5,6 +5,9 @@ using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
+using MonoGame.Extended.Sprites;
+using System.IO;
+using System;
 
 namespace TEMEliminatesMonsters
 {
@@ -16,11 +19,12 @@ namespace TEMEliminatesMonsters
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
         private OrthographicCamera _camera;
+        private Texture2D _smile;
+        private Sprite _smileSprite;
 
         public TEM()
         {
             _graphics = new GraphicsDeviceManager(this);
-            _cameraController = new(_camera, Mouse.GetState().X, Mouse.GetState().Y);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -31,12 +35,14 @@ namespace TEMEliminatesMonsters
 
             BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480);
             _camera = new OrthographicCamera(viewportAdapter);
+            _cameraController = new(_camera, Mouse.GetState().X, Mouse.GetState().Y);
         }
-
         protected override void LoadContent()
         {
             //_tiledMap = Content.Load<TiledMap>("samplemap");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            _smile = Texture2D.FromStream(_graphics.GraphicsDevice, File.Open("C:\\Users\\a.belski\\source\\repos\\TEM\\Content\\bin\\Windows\\Content\\smile.png", FileMode.Open));
+            
+            //_tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
 
             _spriteBatch = new SpriteBatch(GraphicsDevice);
         }
@@ -46,18 +52,18 @@ namespace TEMEliminatesMonsters
 
             base.Update(gameTime);
             _cameraController.Update(gameTime);
-            _tiledMapRenderer.Update(gameTime);
+            //_tiledMapRenderer.Update(gameTime);
 
         }
 
         protected override void Draw(GameTime gameTime) // Note, do NOT move gameobjects in here 
         {
-            GraphicsDevice.Clear(Color.Black);
-            _tiledMapRenderer.Draw();
+            GraphicsDevice.Clear(Color.LightBlue);
+            //_tiledMapRenderer.Draw();
 
             var transformMatrix = _camera.GetViewMatrix();
             _spriteBatch.Begin(transformMatrix: transformMatrix);
-            _spriteBatch.DrawRectangle(new RectangleF(300, 300, 25, 25), Color.Black, 12.5f);
+            _spriteBatch.Draw(_smile, new Vector2(150, 150), Color.Red);
             _spriteBatch.End();
 
             base.Draw(gameTime);
