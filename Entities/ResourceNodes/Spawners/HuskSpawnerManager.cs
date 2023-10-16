c9d22a37a949e453c56d826dbe6ba3364be3fcb8
zelzmiy <adambelski2@gmail.com>
2023-10-16 11:13:16 -0500

    test :)

---
+++ Entities/ResourceNodes/Spawners/HuskSpawnerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.Entities;
+
+namespace TEMEliminatesMonsters.Entities.ResourceNodes.Spawners
+{
+    internal class HuskSpawnerManager
+    {
+    }
+}
