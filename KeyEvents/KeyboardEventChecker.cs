9c30f814ac286ec47e0258ed8d0368ebee6d85d2
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2023-10-18 20:00:50 -0500

    working keyboard events!!!!

---
+++ KeyEvents/KeyboardEventChecker.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+
+namespace TEMEliminatesMonsters.KeyEvents
+{
+    static class KeyboardEventChecker
+    {
+        static KeyboardState _currentKeyState, _previousKeyState;
+
+        public static KeyboardState GetState()
+        {
+            _previousKeyState = _currentKeyState;
+            _currentKeyState = Keyboard.GetState();
+            return _currentKeyState;
+        }
+
+        public static bool IsPressed(Keys key)
+        {
+            return _currentKeyState.IsKeyDown(key);
+        }
+
+        public static bool HasBeenPressed(Keys key)
+        {
+            return _currentKeyState.IsKeyDown(key) && !_previousKeyState.IsKeyDown(key);
+        }
+        public static void Update()
+        {
+            GetState();
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                if (HasBeenPressed(key))
+                {
+                    KeyboardEventManager.GetEvent(key)?.Invoke();
+                    Debug.WriteLine($"{key} event Invoked!");
+                }
+            }
+        }
+    }
+}
---
+++ KeyEvents/KeyboardEventManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEMEliminatesMonsters.KeyEvents
+{
+    public static class KeyboardEventManager
+    {
+
+        public static Dictionary<Keys, KeyEvent> KeyboardEvents;
+
+        static KeyboardEventManager() 
+        {
+            KeyboardEvents = new Dictionary<Keys, KeyEvent>();
+            CreateKeyEvents();
+        }
+
+        private static void CreateKeyEvents()
+        {
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                KeyboardEvents.Add(key, new());
+            }
+        }
+
+        public static ref Action GetEvent(Keys key) 
+        {
+            KeyboardEvents.TryGetValue(key, out KeyEvent keyEvent);
+            return ref keyEvent.GetEvent();
+        }
+    }
+}
+
+
+
