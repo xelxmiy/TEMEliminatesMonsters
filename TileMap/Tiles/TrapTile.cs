9b2221a9fe1b6004fbc7ad8f2523fa96192d417a
zelzmiy <adambelski2@gmail.com>
2023-11-13 11:41:37 -0600

    something something rename and colison..

---
+++ TileMap/Tiles/TrapTile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Collections;
+using MonoGame.Extended.Collisions;
+using TEMEliminatesMonsters.Updateables;
+
+namespace TEMEliminatesMonsters.TileMap.Tiles
+{
+    abstract class TrapTile : Tile, ICollisionActor
+    {
+        public TrapTile(Texture2D texture, int x, int y, int? ID = null) : base(texture, x, y, ID)
+        {
+            (this as Updateables.IUpdateable).AddSelfToUpdateables();
+            if (texture != null)
+            {
+                _bounds = new RectangleF(new(x*32,y*32), new(texture.Width * _tileSizeMultiplier, texture.Height * _tileSizeMultiplier));
+            }
+        }
+
+        private IShapeF _bounds;
+
+        IShapeF ICollisionActor.Bounds => _bounds;
+
+        public void OnCollision(CollisionEventArgs collisionInfo)
+        {
+        
+        }
+    }
+}
